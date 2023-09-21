c5c0a4c2941ee4dbd3bd8cec7caadd83e6e0d215
Sfonseca24 <santiagofg246@gmail.com>
2023-09-20 21:38:07 -0500

    añadir objectos OBJ y MTL
    se añadieron los objetos MTL y OBJ  y se procedio a acomodar la carpeta y archivos

---
+++ src/JsCompu/mainObjexts.js
@@ -0,0 +1,92 @@
+/*
+    author: Santiago Fonseca
+    last date of creation: 23/08/2023 9:20 pm
+    last modification: 29/08/2023 11:54 pm
+*/
+
+// creation elements
+
+var scene = null,
+    camera = null,
+    renderer = null,
+    control = null;
+
+function startScene() {
+    // Scene, Camera, Renderer
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x000); //33FFC5
+    camera = new THREE.PerspectiveCamera(
+                                            75,                                        //Angulo de visión(Abajo o arriba) 
+                                            window.innerWidth / window.innerHeight,    //Relación de aspecto 16:9
+                                            0.1,                                       //Mas cerca (no renderiza)
+                                            1000);                                    //Mas lejos ()
+
+    renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('Models3d') });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    //Orbit controls
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    camera.position.set(0, 0, 20);
+    controls.update();
+
+    //Grid Helper
+    // const gridHelper = new THREE.GridHelper(size, divisions);
+    // scene.add(gridHelper);
+
+    //Axes Helper
+    // const axesHelper = new THREE.AxesHelper(5);
+    // scene.add(axesHelper);
+
+    const lightAmbient = new THREE.AmbientLight( 0xFFFFFF ); // soft white light
+    scene.add( lightAmbient );
+
+    // const light = new THREE.PointLight( 0xffffff, 1, 100 );
+    // light.position.set( 5,10,10 );
+    // scene.add( light );
+
+    animate();
+    // Escenario
+    loadModel_objMtl("../src/models/obj_mtl/Escenario/","escenario.obj","escenario.mtl");
+    // Human Model
+    loadModel_objMtl("../src/models/obj_mtl/Personaje/","PersonajeOso2.obj","PersonajeOso2.mtl");
+    // Robot Model
+    loadModel_objMtl("../src/models/obj_mtl/Personaje/","PersonajeRobot2.obj","PersonajeRobot2.mtl");
+}
+
+function animate() {
+
+    requestAnimationFrame(animate);
+    controls.update();
+    renderer.render(scene, camera);
+}
+
+window.addEventListener('resize', onWindowResize, false);
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function loadModel_objMtl(path, nameObj, nameMtl) {
+    //load Mtl
+    var mtlLoader = new THREE.MTLLoader();
+    mtlLoader.setResourcePath(path);
+    mtlLoader.setPath(path);
+    mtlLoader.load(nameMtl, function (materials) {
+        materials.preload();
+
+        //load OBJ
+        var objLoader = new THREE.OBJLoader();
+        objLoader.setPath(path);
+        objLoader.setMaterials(materials);
+        objLoader.load(nameObj, function (object) {
+            scene.add(object);
+            object.scale.set(2,2,2);
+        })
+    })
+
+
+}
